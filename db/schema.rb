62c27aadb83fd060ac0d643b2ea8585bd55cd100
Mrk8 <marketing801@gmail.com>
2016-02-19 09:06:27 -0800

    Created user model, and migrated db.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218172004) do
+ActiveRecord::Schema.define(version: 20160219170227) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 20160218172004) do
     t.datetime "finished_at"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
