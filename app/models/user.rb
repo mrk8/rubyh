9219cb1453412ea198fa450e7aa0171d823499c6
Mrk8 <marketing801@gmail.com>
2016-02-19 23:59:37 -0800

    Adds user model with unique email addresses, and secure passwords with bcrypt gem. Tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+	# callbacks
+	before_save {self.email = email.downcase } 
+
+	# contants
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	# validators 
+	validates :name, presence: true, length:  {maximum: 50}
+	validates :email, presence: true, length: {maximum: 255},
+					  format: { with: VALID_EMAIL_REGEX },
+					  uniqueness: {case_sensitive: false}
+	# check password with bcrypt: https://www.railstutorial.org/book/modeling_users (section 6.34) 
+	has_secure_password				  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  
+	def setup
+		@user = User.new(name: "Example User", email: "user@example.com",
+			password: "foobar", password_confirmation: "foobar")
+	end
+
+	test "should be valid" do  
+		assert @user.valid?	
+	end #end "should be valid" test.
+
+	test "name should be present" do
+		@user.name = "a" * 51
+		assert_not @user.valid?
+	end #end "name should be present" test.
+
+	test "email should be present" do
+		@user.email = "a" * 244 + "@example.com"
+		assert_not @user.valid?
+	end # end "email should be present" test.
+
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_addresses|
+			@user.email = valid_addresses
+			assert @user.valid?, "#{valid_addresses.inspect} should be valid"
+		end #end valid_addresses.each 
+	end #end "email validation should accept valid addresses" test.
+
+	test "email address must be unique" do
+		duplicate_user = @user.dup
+		duplicate_user.email = @user.email.upcase
+		@user.save
+		assert_not duplicate_user.valid?
+	end #end  "email address must be unique" test.
+
+end #end class.
