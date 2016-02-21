96fe1399d7fbcc75b367530a732c07ce2b4b0786
Mrk8 <marketing801@gmail.com>
2016-02-21 05:58:42 -0800

    Adds flash alerts to view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
 	  @user = User.new(user_params)
 	 
 	  if @user.save
+	  	 flash[:success] = "Your account has been created."
 	     redirect_to @user
 	  else
 	    render 'new'
