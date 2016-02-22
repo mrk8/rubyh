b76f52f37a0f93e58b17ae96c14ea230f2b62de7
Mrk8 <marketing801@gmail.com>
2016-02-21 22:52:47 -0800

    Added 'remember me' to user login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	# callbacks
 	before_save {self.email = email.downcase } 
 
@@ -12,5 +13,34 @@ class User < ActiveRecord::Base
 					  uniqueness: {case_sensitive: false}
 	# check password with bcrypt: https://www.railstutorial.org/book/modeling_users (section 6.34) 
 	has_secure_password		
-	validates :password, presence: true, length: {minimum: 6}		  
+	validates :password, presence: true, length: {minimum: 6}		
+
+	#  returns the hash digest of the given string. 
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
+													  BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+	# returns a random token
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	def authenticated? (remember_token)
+		return false if remember_digest.nill?
+		Bcrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	# Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220075148) do
+ActiveRecord::Schema.define(version: 20160222044226) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160220075148) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
